9b9dae9c187d1ec01180f021814484cb2d6cb4e2
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-11-03 17:28:11 -0300

    Adding application

--- ping_pong/mix.exs
+++ ping_pong/mix.exs
@@ -14,7 +14,8 @@ defmodule PingPong.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger],
+     mod: {PingPong, []}]
   end
 
   # Dependencies can be Hex packages:
